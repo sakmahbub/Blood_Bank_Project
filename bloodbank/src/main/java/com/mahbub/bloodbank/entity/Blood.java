98be31c68a73137d6f5c00a2db2d3be0c5c48977
Sakil Mahbub <sakmahbub@gmail.com>
2018-12-22 14:10:25 +0600

    some update

--- bloodbank/src/main/java/com/mahbub/bloodbank/entity/Blood.java
+++ bloodbank/src/main/java/com/mahbub/bloodbank/entity/Blood.java
@@ -28,4 +28,7 @@ public class Blood {
     public void setBloodName(String bloodName) {
         this.bloodName = bloodName;
     }
+
+
+
 }
--- bloodbank/src/main/java/com/mahbub/bloodbank/entity/Request.java
+++ bloodbank/src/main/java/com/mahbub/bloodbank/entity/Request.java
@@ -14,8 +14,44 @@ public class Request {
     private String quantity;
 
 
-    @OneToOne
+    @ManyToOne
     @JoinColumn(name = "patient_id", nullable =false)
     private Patient patient;
 
+
+    @ManyToOne
+    @JoinColumn(name = "hospital_id", nullable =false)
+    private Hospital hospital;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(String quantity) {
+        this.quantity = quantity;
+    }
+
+    public Patient getPatient() {
+        return patient;
+    }
+
+    public void setPatient(Patient patient) {
+        this.patient = patient;
+    }
+
+    public Hospital getHospital() {
+        return hospital;
+    }
+
+    public void setHospital(Hospital hospital) {
+        this.hospital = hospital;
+    }
 }
