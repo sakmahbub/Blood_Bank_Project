11590849296062a53767debc46abef621e5c6cee
Sakil Mahbub <sakmahbub@gmail.com>
2018-12-27 14:28:39 +0600

    some update

---
+++ bloodbank/src/main/java/com/mahbub/bloodbank/controller/RoleController.java
@@ -0,0 +1,49 @@
+package com.mahbub.bloodbank.controller;
+
+
+import com.mahbub.bloodbank.entity.Role;
+import com.mahbub.bloodbank.repo.RoleRepo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.validation.Valid;
+
+@Controller
+@RequestMapping(value = "/role/")
+public class RoleController {
+
+    @Autowired
+    private RoleRepo repo;
+
+    @GetMapping(value = "create.jsf")
+    public String displayRole(Model model){
+        model.addAttribute("obj", new Role());
+        return "role/create";
+    }
+
+    @GetMapping(value = "list.jsf")
+    public String getRoleList(Model model){
+        model.addAttribute("obj", new Role());
+        return "role/create";
+    }
+
+
+
+    @PostMapping(value = "create.jsf")
+    public String saveRole(@Valid Role obj, BindingResult result, Model model){
+     if(obj !=null) {
+         repo.save(obj);
+         model.addAttribute("successMsg", "Success");
+         model.addAttribute("obj", new Role());
+     }
+        return "role/create";
+    }
+
+
+
+}
---
+++ bloodbank/src/main/java/com/mahbub/bloodbank/repo/RoleRepo.java
@@ -0,0 +1,10 @@
+package com.mahbub.bloodbank.repo;
+
+import com.mahbub.bloodbank.entity.Role;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface RoleRepo extends JpaRepository<Role, Long> {
+
+}
